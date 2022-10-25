3c388c90bcd65bddbadbb5f0759d94802405d273
pranathisadhula <pranathisadhula@gmail.com>
2022-10-26 01:00:51 +0530

    reorg folder structure

--- datastructures/datastructures/src/main/java/week2/daywise/assignments/Assignments.java
+++
@@ -1,9 +0,0 @@
-package week2.daywise.assignments;
-
-public class Assignments {
-/**
- * https://leetcode.com/problems/intersection-of-two-arrays/
-https://leetcode.com/problems/shortest-distance-to-a-character/
-	https://leetcode.com/problems/merge-strings-alternately/
- */
-}
