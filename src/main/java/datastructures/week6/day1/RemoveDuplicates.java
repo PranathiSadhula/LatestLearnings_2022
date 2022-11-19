d75d810f98bbe159422e8c40a499ccc9a6ed950c
PranathiSadhula <pranathisadhula@gmail.com>
2022-11-19 18:46:41 +0530

    Implemented deleteDuplicates from a given sorted LL values.

---
+++ src/main/java/datastructures/week6/day1/RemoveDuplicates.java
@@ -0,0 +1,69 @@
+package datastructures.week6.day1;
+
+import org.junit.Test;
+
+public class RemoveDuplicates {
+    /**
+     * Given the head of a sorted linked list,
+     * delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+     *
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: head = [1,1,2]
+     * Output: [1,2]
+     * Example 2:
+     *
+     *
+     * Input: head = [1,1,2,3,3]
+     * Output: [1,2,3]
+     *
+     *
+     * Constraints:
+     *
+     * The number of nodes in the list is in the range [0, 300].
+     * -100 <= Node.val <= 100
+     * The list is guaranteed to be sorted in ascending order.
+     */
+
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    class ListNode {
+        Integer nodeValue;
+        ListNode nextNode;
+        ListNode() {};
+        ListNode(Integer nodeValue) {
+            this.nodeValue = nodeValue;
+         }
+        ListNode(int val, ListNode next) {
+            this.nodeValue = val;
+            this.nextNode = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp =  head;
+        while(temp != null){
+            if(temp.nodeValue == temp.nextNode.nodeValue){
+                temp.nextNode = temp.nextNode.nextNode;
+            }
+            temp = temp.nextNode;
+        }
+        return  head;
+    }
+
+
+
+}
